cbdf1955fc7ba01ad31d5363359f844d7a25024b
Thành Tài <taint0812@gmail.com>
2023-04-16 19:07:56 +0700

    SM-02 | Implement class MonHoc and methods

--- StudentManagement/MonHoc.h
+++ StudentManagement/MonHoc.h
@@ -1,5 +1,29 @@
 #pragma once
+#include <string>
+using std::string;
+
 class MonHoc
 {
+private:
+	static int ma;
+	string maMonHoc;
+	int soTinChi;
+	string tenMonHoc;
+	bool daXoa;
+public:
+	MonHoc() {
+		this->maMonHoc = ma++;
+		this->daXoa = false;
+	}
+	MonHoc(int soTinChi, string tenMonHoc) {
+		this->maMonHoc = ma++;
+		this->soTinChi = soTinChi;
+		this->tenMonHoc = tenMonHoc;
+		this->daXoa = false;
+	}
+	void Nhap();
+	void Xuat();
+	void Sua(string tenMH, int soTC);
+	void Xoa();
 };
 
