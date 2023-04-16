6a5be6a073ade44db87ac9ea9128910ca1605283
Thành Tài <taint0812@gmail.com>
2023-04-16 19:24:37 +0700

    SM-03 | Implement class Lophoc and methods

--- StudentManagement/LopHoc.cpp
+++ StudentManagement/LopHoc.cpp
@@ -1,1 +1,39 @@
 #include "LopHoc.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+void LopHoc::Nhap() {
+	cin.ignore();
+	cout << "Nhap ma lop hoc: ";
+	cin >> maLopHoc;
+	cout << "Nhap ten lop hoc: ";
+	cin >> tenLopHoc;
+	cout << "Nhap ma mon hoc: ";
+	cin >> maMonHoc;
+	cout << "Nhap tiet bat dau: ";
+	cin >> tietBatDau;
+	cout << "Nhap tiet ket thuc : ";
+	cin >> tietKetThuc;
+}
+
+void LopHoc::Xuat() {
+	cout << "Ma ma lop hoc: " << maLopHoc << endl;
+	cout << "Ten lop hoc: " << tenLopHoc << endl;
+	cout << "Ma mon hoc: " << maMonHoc << endl;
+	cout << "Tiet bat dau: " << tietBatDau << endl;
+	cout << "Tiet ket thuc: " << tietKetThuc << endl;
+}
+
+void LopHoc::Sua(string maLH, string tenLH, string maMH, int tietBD, int tietKT) {
+	maLopHoc = maLH;
+	tenLopHoc = tenLH;
+	maMonHoc = maMH;
+	tietBatDau = tietBD;
+	tietKetThuc = tietKT;
+}
+
+void LopHoc::Xoa() {
+	daXoa = true;
+}
+
